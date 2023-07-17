c54efcc9d9f4a9d4dfe303954dd81c4f9e40d719
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-07-17 19:46:38 +0300

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Entry point.
  * Description: prints alphabet in lowercase then uppercase
  * Return: Always 0 (success)
  */
