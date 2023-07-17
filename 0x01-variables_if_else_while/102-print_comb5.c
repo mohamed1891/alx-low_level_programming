461c468645e074fe403a26b98ae98d77c78bdf06
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-07-17 19:52:03 +0300

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Entry point.
  * Description: prints all possible combination of 2-digit numbers
  * Return: Always 0 (success)
  */
