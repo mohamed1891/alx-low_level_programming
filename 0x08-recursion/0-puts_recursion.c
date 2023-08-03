b4fc8a9d840305cc2e07f289dfbb4f548b47fd07
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-08-04 02:31:40 +0300

    fix code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point.
+ * @s: the string.
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point.
+ * @s: the string.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
