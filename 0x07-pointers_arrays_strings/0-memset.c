f1172e66639b18ecb315d85b697a9376c7a97e02
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-08-16 17:33:40 +0300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,13 @@
 
 /**
  * _memset - Entry point.
- * @s: pointer.
- * @b: buffer.
+ * @s: pointer
+ * @b: buffer
  * @n: number of bytes to write.
  * Return: buffer.
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	return(memset(s, b, n,));
 }
