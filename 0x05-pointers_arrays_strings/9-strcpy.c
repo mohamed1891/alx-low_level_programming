bb342991f5dd3a3c1e3fb901d7c085e2ed5e793a
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-08-23 16:40:57 +0300

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * *_strcpy - Entry point.
+ * @dest: pointer
+ * @src: pointer
+ * Return: pointer to destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
