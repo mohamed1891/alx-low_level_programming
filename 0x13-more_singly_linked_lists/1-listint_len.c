5f6df7338841ed70736633470ab54ff452ab80f0
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-08-29 00:53:24 +0300

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
  * listint_len - Returns the number of elements in a listint_t list.
  * @h: Pointer to the head of the linked list.
  *
- * Return: The number of elements in the list.
+ * Return: The number of elements in the list
  */
 size_t listint_len(const listint_t *h)
 {
