51b149ded63c978c3b7944d141c5f113b59c83fa
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-08-29 15:03:31 +0300

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
-
-# Collect all .c files in the current directory
-c_files=$(ls *.c 2>/dev/null)
-
-# Check if any .c files exist
-if [ -z "$c_files" ]; then
-    echo "No .c files found in the current directory."
-    exit 1
-fi
-
-# Compile each .c file into an object file
-for file in $c_files; do
-    gcc -c "$file" -o "${file%.c}.o"
-    if [ $? -ne 0 ]; then
-        echo "Compilation failed for $file"
-        exit 1
-    fi
-done
-
-# Create the static library
+gcc -c *.c
 ar rcs liball.a *.o
-
-# Check if library creation was successful
-if [ $? -eq 0 ]; then
-    echo "Static library liball.a created successfully."
-else
-    echo "Failed to create the static library."
-fi
-
-# Clean up object files
-rm -f *.o
+ranlib liball.a
+rm *.o
