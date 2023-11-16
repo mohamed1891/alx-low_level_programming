24afd9954f5741ef0916008e9cdb6744b88c8a8a
mohamed1891 <ma.elmoneim1991@gmail.com>
2023-11-16 03:18:26 +0200

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,16 +13,16 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
-		return 0;
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
-	bytes_read = read(fd, &buf[0], letters);
-	bytes_written = write(STDOUT_FILENO, &buf[0], bytes_read);
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-	return bytes_written;
+	return (bytes);
 }
