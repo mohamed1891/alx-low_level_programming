bd964510054ed55d849a991a4de0318fcd189c0f
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-06 15:11:57 +0200

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,28 +13,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    /* declare a pointer of type char */
-    char *ptr;
+	/* declare a pointer of type char */
+	char *ptr;
 
-    /* check if s1 or s2 are NULL and treat them as empty strings */
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	/* check if s1 or s2 are NULL and treat them as empty strings */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    /* allocate memory for the concatenated string */
-    ptr = malloc(strlen(s1) + (n < strlen(s2) ? n : strlen(s2)) + 1);
+	/* allocate memory for the concatenated string */
+	ptr = malloc(strlen(s1) + (n < strlen(s2) ? n : strlen(s2)) + 1);
 
-    /* check if malloc failed */
-    if (ptr == NULL)
-        return NULL;
+	/* check if malloc failed */
+	if (ptr == NULL)
+		return (NULL);
 
-    /* copy s1 to the pointer */
-    strcpy(ptr, s1);
+	/* copy s1 to the pointer */
+	strcpy(ptr, s1);
 
-    /* concatenate s2 to the pointer */
-    strncat(ptr, s2, n);
+	/* concatenate s2 to the pointer */
+	strncat(ptr, s2, n);
 
-    /* return the pointer */
-    return ptr;
+	/* return the pointer */
+	return (ptr);
 }
