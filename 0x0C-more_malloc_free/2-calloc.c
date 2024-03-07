37a9b50bf428e5700a565ba1fa991386ae9b36d5
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-07 14:57:20 +0200

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,23 +11,23 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    /* declare a pointer of type void */
-    void *ptr;
+	/* declare a pointer of type void */
+	void *ptr;
 
-    /* check if nmemb or size is 0 and return NULL */
-    if (nmemb == 0 || size == 0)
-        return NULL;
+	/* check if nmemb or size is 0 and return NULL */
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    /* allocate memory for the array using malloc */
-    ptr = malloc(nmemb * size);
+	/* allocate memory for the array using malloc */
+	ptr = malloc(nmemb * size);
 
-    /* check if malloc failed */
-    if (ptr == NULL)
-        return NULL;
+	/* check if malloc failed */
+	if (ptr == NULL)
+		return (NULL);
 
-    /* set the memory to zero using memset */
-    memset(ptr, 0, nmemb * size);
+	/* set the memory to zero using memset */
+	memset(ptr, 0, nmemb * size);
 
-    /* return the pointer */
-    return ptr;
+	/* return the pointer */
+	return (ptr);
 }
