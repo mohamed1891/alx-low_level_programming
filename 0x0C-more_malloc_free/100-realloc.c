50138d7bca345fdb193fca696eea4d1196fb8b01
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-03-09 12:21:27 +0200

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,44 +15,37 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy;
-
+	void *new_ptr;
 	/**
-	 * If the new size is the same as the old size, free the original pointer
-	 * and allocate a new block of memory with the requested size.
+	 * If the new size is 0 and the original pointer is not NULL,
+	 * free the original pointer and return NULL.
 	 */
-	if (new_size == old_size)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (malloc(new_size));
+		return (NULL);
 	}
-
 	/* If the original pointer is NULL, simply allocate a new block of memory. */
 	if (ptr == NULL)
+	{
 		return (malloc(new_size));
-
-	/* If the new size is 0, free the original pointer and return NULL. */
-	if (new_size == 0)
+	}
+	/*If the new size is the same as the old size, return the original pointer.*/
+	if (new_size == old_size)
 	{
-		free(ptr);
+		return (ptr);
+	}
+	/* Allocate a new block of memory with the requested size.*/
+	new_ptr = malloc(new_size);
+	/* Check if malloc failed */
+	if (new_ptr == NULL)
+	{
 		return (NULL);
 	}
-
-	/* Allocate a new block of memory with the requested size. */
-	if (mam == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
 	/* Copy the contents from the old block to the new block using memcpy. */
-	ptr_copy = ptr;
-	memcpy(mem, ptr_copy, (old_size < new_size) ? old_size : new_size);
-
-	/* Free the original pointer. */
+	memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+	/* Free the original pointer */
 	free(ptr);
-
 	/* Return the pointer to the newly created or reallocated memory block. */
-	return (mem);
+	return (new_ptr);
 }
